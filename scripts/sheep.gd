50978d12be5c5db8e4771d35096117b595a5e0a9
richard.frolkovic@event-horizon.sk <richard.frolkovic@event-horizon.sk>
2023-11-11 22:39:42 +0100

    Resetting position of sheep when exit screen

--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -1,4 +1,26 @@
 extends RigidBody2D
 class_name Sheep
 
+@export var AppearEffect: AudioStreamPlayer = null
 
+var start_pos = Vector2.ZERO
+var reset = false;
+
+func _ready():
+	start_pos = position
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	if AppearEffect != null:
+		AppearEffect.play()
+	
+	
+func _integrate_forces(state):
+	if reset:
+		state.transform = Transform2D(0, start_pos)
+		reset = false
+
+
+func _on_audio_stream_player_finished():
+	print("I am sad")
+	reset = true
