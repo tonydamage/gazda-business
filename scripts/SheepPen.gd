70942188c498adc76db1edf6de2d9499b988af23
Tony Tkáčik <tonydamage@gmail.com>
2023-11-11 16:38:01 +0100

    Dragable Control
    Signed-off-by: Tony Tkáčik <tonydamage@gmail.com>

--- scripts/SheepPen.gd
+++ scripts/SheepPen.gd
@@ -12,7 +12,7 @@ func _ready():
 			requiredSheep += 1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	var sheepInArea = 0
 	for c in get_overlapping_bodies():
 		if c is Sheep:
--- scripts/sheep.gd
+++ scripts/sheep.gd
@@ -1,22 +1,2 @@
 extends RigidBody2D
 class_name Sheep
-
-@export var runForce = 500
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
-func _on_body_entered(body):
-	if body is SheepCollider:
-		# Change Sheep state
-		var vec = (body.global_position - global_position).normalized()
-		apply_force(vec * -runForce);
-		pass
-	pass # Replace with function body.
