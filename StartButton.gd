a0ca54e90f92e932c8c35513f99833ed856ba3e8
Tony Tkáčik <tonydamage@gmail.com>
2023-11-12 07:33:15 +0100

    Added tutorial level as first
    Signed-off-by: Tony Tkáčik <tonydamage@gmail.com>

--- StartButton.gd
+++ StartButton.gd
@@ -4,6 +4,6 @@ extends Button
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://level/level-base.tscn")
+	get_tree().change_scene_to_file("res://level/tutorial.tscn")
 	if SoundEffect != null:
 		SoundEffect.play()
